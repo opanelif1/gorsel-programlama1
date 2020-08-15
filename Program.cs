3d4ba6693c96e59caf80cbc182da29f388f90385
opanelif1 <64418263+opanelif1@users.noreply.github.com>
2020-08-15 21:55:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ternaryOperator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kitap Okumayı Seviyor musunuz? (e, h): ");
+            string durum = Console.ReadLine();
+            Console.Write(durum == "e" ? "Alışkanlığınız İçin Tebrikler!!" : "Alışkanlık Edinmeniz Sizin İçin En İyisi");
+            Console.ReadLine();
+        }
+    }
+}
