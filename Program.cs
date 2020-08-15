38bd76633dfa8a9b6f0bd7f0d2ac313f036ec38c
opanelif1 <64418263+opanelif1@users.noreply.github.com>
2020-08-15 21:56:20 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ternaryOperator2
+namespace ornek1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Kitap Okumayı Seviyor musunuz? (e, h): ");
-            string durum = Console.ReadLine();
-            Console.Write(durum == "e" ? "Alışkanlığınız İçin Tebrikler!!" : "Alışkanlık Edinmeniz Sizin İçin En İyisi");
+            string s1, s2;
+            int sayi1, sayi2;
+            int Toplam;
+            Console.Write("Birinci sayıyı girin: ");
+            s1 = Console.ReadLine();
+            Console.Write("İkinci sayıyı girin: ");
+            s2 = Console.ReadLine();
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+            Toplam = sayi1 + sayi2;
+            Console.Write("Toplam= " + Toplam);
             Console.ReadLine();
         }
     }
